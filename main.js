cd392ed05fea9234dafc59668977d5aaac67d020
Wendy Nichols <wendlelyn@gmail.com>
2014-09-30 16:40:03 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+
+  //  document.getElementById('')
+
+  var input1 = document.getElementById('num_one'),
+      input2 = document.getElementById('num_two'),
+      button = document.getElementById('calculateBtn'),
+      answer = document.getElementById('answer');
+
+
+  // button1 = document.getElementById('num_one').value;
+
+  //get the value of the name input
+
+  button.onclick = function () {
+
+    var value1 = input1.value;
+    console.log(value1);
+    var value2 = input2.value;
+    console.log(value2);
+    var solution = value1 + value2;
+    console.log(solution);
+
+convert value to numbers parse int or Number
+
+  console.log ('this is making my head hurt');
+    // numbers
+    // math (*,+,-,/)
+    // value of the numbers
+
+  //  var adder = new function("a", "b", "return a + b");
+
+  //  adder(2, 6);
+
+
+  // //add the name value to some greeting
+  // var greeting = "hello, " + name;
+  //
+  // //find the greetingArea element
+  // var greeting_area = document.getElementById('greetingArea');
+  //
+  // //spit the content of my greeting into that area
+  // greeting_area.innerHTML = greeting;
+
+
+  };
